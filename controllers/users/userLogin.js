aa8f9c8e0d8038638aef5b184972c2aa279d85af
D-Bednarczyk <117660494+D-Bednarczyk@users.noreply.github.com>
2023-11-02 20:12:22 +0100

    signup and login

--- controllers/users/userLogin.js
+++ controllers/users/userLogin.js
@@ -1,4 +1,50 @@
+import User from "#service/schemas/users.js";
+import jwt from "jsonwebtoken";
+
+import Joi from "joi";
+import "dotenv/config";
+
+const SECRET = process.env.SECRET;
+
+const schemaValidation = Joi.object({
+  username: Joi.string(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
 export const userLogin = async (req, res, next) => {
-  try {
-  } catch (err) {}
+  const resultValidate = schemaValidation.validate(req.body);
+
+  if (resultValidate.error) {
+    return res.status(400).json({
+      message: resultValidate.error.message,
+    });
+  }
+
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({ message: "Email or password is wrong" });
+  }
+
+  const isPasswordCorrect = await user.validatePassword(password);
+  if (isPasswordCorrect) {
+    const payload = {
+      id: user._id,
+      email: user.email,
+    };
+    const token = jwt.sign(payload, SECRET, { expiresIn: "12h" });
+
+    await User.findByIdAndUpdate(
+      { _id: payload.id },
+      { token: token },
+      { new: true }
+    );
+
+    const { email, subscription } = user;
+    return res.json({ token, user: { email, subscription } });
+  } else {
+    return res.status(401).json({ message: "Email or password is wrong" });
+  }
 };
--- controllers/users/userSignup.js
+++ controllers/users/userSignup.js
@@ -1,4 +1,39 @@
+import User from "#service/schemas/users.js";
+import Joi from "joi";
+
+const schemaValidation = Joi.object({
+  username: Joi.string(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
 export const userSignup = async (req, res, next) => {
+  const resultValidate = schemaValidation.validate(req.body);
+
+  if (resultValidate.error) {
+    return res.status(400).json({
+      message: resultValidate.error.message,
+    });
+  }
+
+  const { username, email, password } = req.body;
+
+  const user = await User.findOne({ email }, { _id: 1 }).lean();
+
+  if (user) {
+    return res.status(409).json({ message: "Email in use" });
+  }
   try {
-  } catch (err) {}
+    const newUser = new User({ username, email });
+    await newUser.setPassword(password);
+    await newUser.save();
+    res.status(201).json({
+      user: {
+        email: email,
+        subscription: "starter",
+      },
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
